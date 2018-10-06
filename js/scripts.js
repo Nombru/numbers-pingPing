a4dc2b346a16d7681fb2e14206bc82efb68b08a4
Nombru <1662220+Nombru@users.noreply.github.com>
2018-10-06 15:25:20 -0700

    add business and user logic, to create funcitoning program. Need to resolve issue with appending results so applicaiton can be used multiple times.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,63 @@
 // Business Logic
 
+function countDown(inputNumber, resultList){
+  for (var index = 1; index <= inputNumber; index += 1){
+    $(resultList).append("<li>" + divisible(index) + " </li>");
+  }
+}
+
+var divisible = function(inputNumber){
+  if ((inputNumber % 15) === 0){
+    return "pingpong"
+  } else if ((inputNumber % 5) === 0){
+    return "pong";
+  } else if ((inputNumber % 3) === 0){
+    return "ping";
+  } else {
+    return inputNumber;
+  }
+}
+
+// var emptyArray = []
+// var total = 0;
+// var countTo = parseInt($("#number1").val());
+// var countBy = parseInt($("#number2").val());
+//
+// var countingNumber = Math.floor(countTo/countBy);
+// 
+// if (isNaN(countingNumber)) {
+//   alert("Enter some numbers");
+// } else {
+//   for (index = 0; index <= countingNumber; index += 1) {
+//     total = index * countBy;
+//     emptyArray.push(total);
+//     $(".results").append(total);
+//     };
+//   };
+
 // User Input Logic
 $(document).ready(function() {
-  $("#input").submit(function(event){
+  $("form#input").submit(function(event){
     event.preventDefault();
     console.log("hi");
+
+    $(".alertmsg").remove();
+        $("#results li").remove();
+
+        var inputNumber = parseInt($("input#numInput").val());
+
+        if (inputNumber !== inputNumber){
+          $(".form-group").append("<span class='alertmsg'>Please enter a valid number.</span>");
+          return false;
+        } else if (inputNumber < 0){
+          $(".form-group").append("<span class='alertmsg'>Please enter a positive integer.</span>");
+          return false;
+        }
+
+        var result = countDown(inputNumber, ".output");
+        $("#output").fadeIn("slow");
+
+
+
+    });
   });
-});
