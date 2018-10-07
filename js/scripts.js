9488595feb2214afa9c87dbfc0eb5de1e3404202
Nombru <1662220+Nombru@users.noreply.github.com>
2018-10-07 13:30:48 -0700

    make sure all edits to code are up to date for the applicaiton to fuciton.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,10 @@
 // Business Logic
 
-// var remove = function(output) {
-//   var element = document.getElementById("#output");
-//   element.parentNode.removeChild(element);
-// }
-
 var countDown = function(inputNumber, resultList){
   for (var index = 1; index <= inputNumber; index += 1){
     $(resultList).append("<li class= 'output_li'>" + divisible(index) + " </li>");
   }
 }
-
-
 
 var divisible = function(inputNumber){
   if ((inputNumber % 15) === 0){
@@ -26,19 +19,16 @@ var divisible = function(inputNumber){
 }
 
 
-
 // User Input Logic
 $(document).ready(function() {
   $("form#input").submit(function(event){
     event.preventDefault();
-    console.log("hi");
-
+    // console.log("hi");
+// used later as a part of the if and else if statements below.
     $(".alertmsg").remove();
+// Used in the countDown variable.
     $(".output_li").remove();
-
-
-        var inputNumber = parseInt($("input#numInput").val());
-
+      var inputNumber = parseInt($("input#numInput").val());
         if (inputNumber !== inputNumber){
           $(".form-group").append("<span class='alertmsg'>Please enter a valid number.</span>");
           return false;
@@ -47,30 +37,7 @@ $(document).ready(function() {
           return false;
         }
 
-        var result = countDown(inputNumber, ".output");
-        $("#output").fadeIn("slow");
-
-
-
-
-
-
-        // var emptyArray = []
-        // var total = 0;
-        // var countTo = parseInt($("#number1").val());
-        // var countBy = parseInt($("#number2").val());
-        //
-        // var countingNumber = Math.floor(countTo/countBy);
-        //
-        // if (isNaN(countingNumber)) {
-        //   alert("Enter some numbers");
-        // } else {
-        //   for (index = 0; index <= countingNumber; index += 1) {
-        //     total = index * countBy;
-        //     emptyArray.push(total);
-        //     $(".results").append(total);
-        //     };
-        //   };
-
+      var result = countDown(inputNumber, ".output");
+      $("#output").fadeIn("slow");
     });
   });
